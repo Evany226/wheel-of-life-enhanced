c2622872254b122a24b7e4e6745b2d10406185d6
Evan Yang <71720197+Evany226@users.noreply.github.com>
2022-04-07 20:50:46 -0400

    print button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector(".themeButton").addEventListener("click", function () {
+document.querySelector("#themeButton").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList.add("dark");
     document.body.classList.remove("light");
@@ -6,6 +6,10 @@ document.querySelector(".themeButton").addEventListener("click", function () {
     document.body.classList.add("light");
     document.body.classList.remove("dark");
   }
+});
+
+document.querySelector("#printButton").addEventListener("click", function () {
+  window.print();
 });
 
 const form = document.querySelector("#form");
